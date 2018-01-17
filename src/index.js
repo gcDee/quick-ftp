10ceb7e683bc9b19f9b22a57106c897522411661
gc <cvd2261@protonmail.com>
2018-01-17 03:15:06 -0500

    failed build

--- src/index.js
+++ src/index.js
@@ -1,29 +1,78 @@
-import { FTPServer } from 'ftpd';
+import "babel-register";
+import ftpd from 'simple-ftp';
+import fs from 'fs';
+import commander from 'commander';
+import { version } from './package.json';
 
-const options = {
-    host: process.env.IP || '127.0.0.1',
-    port: process.env.PORT || 7002,
-    tls: null,
+const program = commander
+    .version(version)
+    .usage('root [options...]')
+    .option('--port [port]', 'set port', (n) => parseInt(n, 10))
+    .option('--host [ipv4]', 'set host')
+    .option('--read-only [bool]', 'set read only mode', v => !(
+        v === 'false' || v === 'no'
+    ))
+    .parse(process.argv);
+
+
+let readOnly = !(program.readOnly === false);
+
+const opts = {
+    root: program.args[0] || process.cwd(),
+    readOnly: readOnly,
+    host: program.host || '127.0.0.1',
+    port: program.port || 1337,
+    maxConnections: program.maxConnections || 10
 };
 
-const server = new FTPServer(options.host, {
-    getInitialCwd: () => {
-        return '/';
-    },
-    getRoot: () => {
-        return process.cwd();
-    },
-    pasvPortRangeStart: 1025,
-    pasvPortRangeEnd: 1050,
-    tlsOptions: options.tls,
-    allowUnauthorizedTls: true,
-    useWriteFile: false,
-    useReadFile: false,
-    uploadMaxSlurpSize: 7000, // N/A unless 'useWriteFile' is true.
+console.log(
+    ' - starting %s ftp server on %s',
+    readOnly ? chalk.green('read only') : chalk.red('full access'),
+    chalk.magenta(opts.root)
+);
+
+const server = ftpd(opts, (session) => {
+    session.on('pass', (username, password, cb) => {
+        cb(); // LET EVERYBODY IN
+    });
+
+    session.on('read', (pathName, offset, cb) => {
+        cb(null, fs.createReadStream(pathName, { start: offset }));
+    });
+
+    session.on('write', (pathName, offset, cb) => {
+        cb(null, fs.createWriteStream(pathName, { start: offset }));
+    });
+
+    session.on('stat', fs.stat);
+    session.on('readdir', fs.readdir);
+    session.on('mkdir', fs.mkdir);
+    session.on('unlink', fs.unlink);
+    session.on('remove', fs.remove);
+    session.on('rename', fs.rename);
 });
 
-server.on('error', () => console.log('FTP Server Error', error));
+server.on('listening', () => {
+    console.log(
+        ' - listening on %s:%s',
+        chalk.blue(opts.host),
+        chalk.yellow(opts.port)
+    );
+});
 
-server.on('client:connected', () => {
-    
-})
+server.on('connection', (socket) => {
+    const { remoteAddress, remotePort } = socket;
+    console.log(
+        ' - received connection from %s:%s',
+        chalk.blue(remoteAddress),
+        chalk.yellow(remotePort)
+    );
+
+    socket.on('end', () => {
+        console.log(
+            ' - client %s:%s disconnected',
+            chalk.blue(remoteAddress),
+            chalk.yellow(remotePort)
+        );
+    });
+});
